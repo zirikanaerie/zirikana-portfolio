99f923cee0f21d1e046e436c7a18e0c45c5e4a8a
zirikanaerie <zirikanaerie358@gmail.com>
2026-09-22 00:51:33 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        target: "https://zirikana-elie.onrender.com",
         changeOrigin: true,
       },
     },
